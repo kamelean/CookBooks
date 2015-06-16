664b4b95e7328b801e05b12759cef9705c9bfad3
Jochen S Emig <jsemig@gmail.com>
2015-06-16 14:55:06 +0800

    added file transfer

--- symfony/recipes/install_pp_au.rb
+++ symfony/recipes/install_pp_au.rb
@@ -9,11 +9,19 @@ obj = s3.buckets['ops-works-config'].objects['au-pp.json']
 file_content = obj.read
 
 # Log output (optional)
-Chef::Log.info(file_content)
+# Chef::Log.info(file_content)
 
 sites = JSON.parse(file_content)
 sites['site'].each do |current|
-  log "I have #{current[1]['user']}" do
-    level :info
+  obj_vhost = s3.buckets['ops-works-config'].objects["vhosts/" + current[1]['vhost']]
+  file_content = obj_vhost.read
+
+  file '/etc/apache2/sites-enabled/' + current[1]['vhost'] do
+    content file_content
+    action :create
   end
+
+  #log "I have #{current[1]['user']}" do
+  #  level :info
+  #end
 end
