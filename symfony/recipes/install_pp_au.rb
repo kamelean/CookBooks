1c4fbaaeec65d3d5fde961158bbedf99529ba119
Jochen S Emig <jsemig@gmail.com>
2015-06-16 17:32:05 +0800

    second file 4

--- symfony/recipes/install_pp_au.rb
+++ symfony/recipes/install_pp_au.rb
@@ -6,39 +6,41 @@ s3 = AWS::S3.new
 vhost_bucket = "vhosts/"
 vhost_path = "/etc/apache2/sites-enabled/"
 
-# Set bucket and object name
-obj = s3.buckets['ops-works-config'].objects['au-pp.json']
+node[:deploy].each do |application, deploy|
+  # Set bucket and object name
+  obj = s3.buckets['ops-works-config'].objects['au-pp.json']
 
 # Read content to variable
-file_content = obj.read
+  file_content = obj.read
 
 # Log output (optional)
 # Chef::Log.info(file_content)
 
-sites = JSON.parse(file_content)
-sites['site'].each do |current|
+  sites = JSON.parse(file_content)
+  sites['site'].each do |current|
 
-  #Get vhost and write to file
-  tmp_vhost_bucket = vhost_bucket + current[1]['vhost']
-  obj_vhost = s3.buckets['ops-works-config'].objects[tmp_vhost_bucket]
-  file_content = obj_vhost.read
-  tmp_vhost_path = vhost_path + current[1]['vhost']
-  file tmp_vhost_path do
-    content file_content
-    action :create
+    #Get vhost and write to file
+    tmp_vhost_bucket = vhost_bucket + current[1]['vhost']
+    obj_vhost = s3.buckets['ops-works-config'].objects[tmp_vhost_bucket]
+    file_content = obj_vhost.read
+    tmp_vhost_path = vhost_path + current[1]['vhost']
+    file tmp_vhost_path do
+      content file_content
+      action :create
+    end
+
+    #{deploy[:deploy_to]}
+
+    #Get web app file
+    obj_app = s3.buckets['ops-works-config'].objects["pp_apps/" + current[1]['web_app']]
+    file_content_app = obj_app.read
+    file "#{deploy[:deploy_to]}/current/web/" + current[1]['web_app'] do
+      content file_content_app
+      action :create
+    end
+
+    #log "I have #{current[1]['user']}" do
+    #  level :info
+    #end
   end
-
-  #{deploy[:deploy_to]}
-
-  #Get web app file
-  obj_app = s3.buckets['ops-works-config'].objects["pp_apps/" + current[1]['web_app']]
-  file_content_app = obj_app.read
-  file "#{deploy[:deploy_to]}/current/web/" + current[1]['web_app'] do
-    content file_content_app
-    action :create
-  end
-
-  #log "I have #{current[1]['user']}" do
-  #  level :info
-  #end
 end
