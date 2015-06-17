abe1f2f79b6f82b061f0e2a4b2355e0bbdddade3
Jochen S Emig <jsemig@gmail.com>
2015-06-17 10:31:33 +0800

    added more modules 3

--- php_kam/recipes/mod_memcache.rb
+++ php_kam/recipes/mod_memcache.rb
@@ -1,16 +1,4 @@
-case node['platform_family']
-  when 'rhel', 'fedora'
-    %w{ zlib-devel }.each do |pkg|
-      package pkg do
-        action :install
-      end
-    end
-    php_pear 'memcache' do
-      action :install
-      # directives(:shm_size => "128M", :enable_cli => 0)
-    end
-  when 'ubuntu'
-    package 'php5-memcached' do
-      action :install
-    end
-end
+
+  package 'php5-memcached' do
+    action :install
+  end
