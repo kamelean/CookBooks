0e719a50582a20d82cbf8ffec3c00794191ba087
Jochen S Emig <jsemig@gmail.com>
2015-06-18 16:40:28 +0800

    add deploy

--- phpmod/recipes/deploy.rb
+++ phpmod/recipes/deploy.rb
@@ -1,4 +1,7 @@
+include_recipe 'deploy'
+
 node[:deploy].each do |application, deploy|
+  Chef::Log.info 'opsworks_deploy going_on'
   opsworks_deploy do
     deploy_data deploy
     app application
