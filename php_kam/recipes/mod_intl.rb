6512956b68f56c1708cf17c809ed7c2ec358dbc7
Jochen S Emig <jsemig@gmail.com>
2015-06-18 09:41:04 +0800

    updated test

--- php_kam/recipes/mod_intl.rb
+++ php_kam/recipes/mod_intl.rb
@@ -1,4 +1,6 @@
-Chef::Log.info 'Installing php5-intl'
-package 'php5-intl' do
-  action :install
+node[:deploy].each do |application, deploy|
+  Chef::Log.info 'Installing php5-intl'
+  package 'php5-intl' do
+    action :install
+  end
 end
