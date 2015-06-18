17ea51622f6337334a1075347dadc4d2dedf9d28
Jochen S Emig <jsemig@gmail.com>
2015-06-18 15:44:20 +0800

    rest

--- phpmod/recipes/intl.rb
+++ phpmod/recipes/intl.rb
@@ -1,17 +1,6 @@
 node[:deploy].each do |application, deploy|
   Chef::Log.info 'Installing php5-intl'
-
-  script "install_php_mods" do
-    interpreter "bash"
-    user "root"
-    cwd "#{deploy[:deploy_to]}/current"
-    code <<-EOH
-    apt-get install php5-intl
-
-    EOH
+  package 'php5-intl' do
+    action :install
   end
-
-  #package 'php5-intl' do
-  #  action :install
-  #end
 end
