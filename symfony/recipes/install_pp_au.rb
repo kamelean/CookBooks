b668151b4978ad277244315503a295d041c2364d
Jochen S Emig <jsemig@gmail.com>
2015-06-21 21:23:49 +0800

    added migrations (again)

--- symfony/recipes/install_pp_au.rb
+++ symfony/recipes/install_pp_au.rb
@@ -88,7 +88,7 @@ node[:deploy].each do |application, deploy|
 
         php #{current[1]['app_folder']}/console assetic:dump --env=#{current[1]['env']}
         php #{current[1]['app_folder']}/console assets:install --env=#{current[1]['env']}
-
+        php #{current[1]['app_folder']}/console doctrine:migrations:migrate --no-interaction --env=#{current[1]['env']}
         php #{current[1]['app_folder']}/console cache:clear --env=#{current[1]['env']} --no-debug --no-warmup
         chmod -R 777 #{current[1]['app_folder']}/cache
         chmod -R 777 #{current[1]['app_folder']}/logs
