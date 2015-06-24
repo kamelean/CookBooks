64519c880d5bdf962e714a5564b59b944493bd04
Jochen S Emig <jsemig@gmail.com>
2015-06-24 13:25:50 +0800

    added stuff

--- symfony/recipes/install_prod_eu.rb
+++ symfony/recipes/install_prod_eu.rb
@@ -59,7 +59,9 @@ node[:deploy].each do |application, deploy|
     new_contents = new_contents.gsub("URL", current[1]['url'])
     new_contents = new_contents.gsub("APP", current[1]['web_app'])
     new_contents = new_contents.gsub("LOG", current[1]['app_folder'])
-    file_content = new_contents.gsub("SSL2", current[1]['env'])
+    new_contents = new_contents.gsub("SSL2", current[1]['env'])
+    new_contents = new_contents.gsub("NORMALPORT", current[1]['env'])
+    file_content = new_contents.gsub("SSLPORT", current[1]['env'])
 
     tmp_vhost_path = vhost_path + current[1]['vhost']
     file tmp_vhost_path do
@@ -68,7 +70,7 @@ node[:deploy].each do |application, deploy|
     end
 
     #Get web app file
-    obj_app = s3.buckets['ops-works-config'].objects["pp_apps/" + current[1]['web_app']]
+    obj_app = s3.buckets['ops-works-config'].objects["prod_apps/" + current[1]['web_app']]
     file_content_app = obj_app.read
     file "#{deploy[:deploy_to]}/current/web/" + current[1]['web_app'] do
       content file_content_app
