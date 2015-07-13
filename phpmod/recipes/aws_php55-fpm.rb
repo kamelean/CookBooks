a5b748df5dedc0bae962ca3393034906e2ac5426
Jochen S Emig <jsemig@gmail.com>
2015-07-13 11:40:41 +0800

    starting php fpm

--- phpmod/recipes/aws_php55-fpm.rb
+++ phpmod/recipes/aws_php55-fpm.rb
@@ -3,4 +3,8 @@ node[:deploy].each do |application, deploy|
   package 'php55-fpm' do
     action :install
   end
+  Chef::Log.info 'Starting php55-fpm'
+  service "php-fpm" do
+    action :start
+  end
 end
